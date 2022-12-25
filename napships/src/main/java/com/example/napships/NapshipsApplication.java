6f31a772b4796a942e69b27a40767f2456d76e32
VermassenJonas <jonas42vermassen@gmail.com>
2022-12-25 11:48:05 +0100

    added springboot project

---
+++ napships/src/main/java/com/example/napships/NapshipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.napships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NapshipsApplication.class, args);
+	}
+
+}
